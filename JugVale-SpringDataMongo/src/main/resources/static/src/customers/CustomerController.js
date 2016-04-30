b2eecbcf869957e8ae265b7cab61c274beabae32
Gabriel Matos <gabrielbiga@me.com>
2016-04-30 01:28:02 -0300

    Added new frontend files

---
+++ JugVale-SpringDataMongo/src/main/resources/static/src/customers/CustomerController.js
@@ -0,0 +1,119 @@
+(function(){
+
+  angular
+       .module('customers')
+       .controller('CustomerController', [
+           'customerService', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log', '$scope', '$mdDialog',
+          CustomerController
+       ]);
+
+  /**
+   * Main Controller
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+   function CustomerController( customerService, $mdSidenav, $mdBottomSheet, $timeout, $log, $scope, $mdDialog) {
+    var self = this;
+
+    self.customers            = [];
+    self.newCustomerName  = "";
+
+    //Initial position for MAP
+    self.map = {
+      center: {
+        latitude: -23.2206729,
+        longitude: -45.8865205
+      },
+      zoom: 15,
+      bounds : {}
+    };
+
+    //Selector
+    self.marker = {
+	    id: 0,
+	    coords: {
+	      latitude: -23.2206729,
+	      longitude: -45.8865205
+	    },
+	    options: {draggable: true,icon: {
+	        path: google.maps.SymbolPath.CIRCLE,
+	        scale: 0
+	    }, labelClass: 'label' },
+	    events: {
+	      dragend: function (marker, eventName, args) {
+	          console.log(self.marker.coords);
+	      },
+	      click : function(marker, eventName, args)
+	      {
+	          if (marker.model.id == undefined) return;
+	          
+	          materialAlert('Pino', "Eu sou: " + marker.model.name);
+	          
+	          console.log(marker.model);
+	      }
+	    }
+    };
+
+    // Load all registered customers
+    customerService
+      .loadAllCustomers( errorNewCustomer )
+      .then( function( customers ) {
+        self.customers = [].concat(customers);
+      });
+
+    // Send new customer
+    self.newCustomer = function() {
+        if( self.newCustomerName == '' ) 
+          return materialAlert("Atenção", "Preencha o nome!");
+
+        var customer = {
+            id        : Math.floor(Math.random() * 9999) +1,
+            name      : self.newCustomerName,
+            latitude  : self.marker.coords.latitude,
+            longitude : self.marker.coords.longitude
+        };
+
+        self.customers.push(customer);
+        
+        storeNewCustomer(customer);
+
+        self.newCustomerName = ""; 
+    }
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    function storeNewCustomer(customer) {
+		var dbCustomer = {
+		  firstName : customer.name,
+		  location  : [ customer.longitude, customer.latitude ]
+		};
+		
+        return customerService.postNewCustomer(dbCustomer)
+                          .then(successNewCustomer, errorNewCustomer);
+    }    
+    
+    function successNewCustomer() {
+    	materialAlert("Legal", "Cadatrado com sucesso!");
+    }
+    
+    function errorNewCustomer() {
+    	materialAlert("Error", "Problemas :(");
+    }
+    
+    function materialAlert(title, text) {
+    	$mdDialog.show(
+  	      $mdDialog.alert()
+  	        .clickOutsideToClose(true)
+  	        .title(title)
+  	        .textContent(text) 
+  	        .ok('Beleza!')
+  	    );
+    }
+
+  }
+
+})();
---
+++ JugVale-SpringDataMongo/src/main/resources/static/src/customers/Customers.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  // Prepare the 'customers' module for subsequent registration of controllers and delegates
+  angular.module('customers', [ 'ngMaterial' ]);
+
+
+})();
